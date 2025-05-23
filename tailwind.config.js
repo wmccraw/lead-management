911bbbcead145eaaf0f8a6ecc85df53d82115404
Wilson McCraw <209520405+wmccraw@users.noreply.github.com>
2025-05-23 11:38:13 -0500

    Forced Tailwind to include key classes and fixed warning

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/index.html"
+  ],
+  safelist: [
+    'bg-gray-100',
+    'text-3xl',
+    'font-bold',
+    'text-gray-800',
+    'mt-4',
+    'hidden',
+    'bg-green-500',
+    'text-white',
+    'px-4',
+    'py-2',
+    'rounded',
+    'hover:bg-green-600'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
